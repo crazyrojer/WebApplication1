f887de5a56ff9de96f415c08658a86152075549f
Andrew <andre.meshkov@gmail.com>
2023-01-19 15:55:56 +0300

    test

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<User> users = new List<User>();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+            users.Add(new WebApplication1.User(1, "Vlad"));
+            users.Add(new WebApplication1.User(2, "Andrey"));
+            users.Add(new WebApplication1.User(3, "Vlad"));
+            users.Add(new WebApplication1.User(4, "Nastya"));
+            users.Add(new WebApplication1.User(5, "Nastya"));
+            users.Add(new WebApplication1.User(6, "Sasha"));
+
+        }
+
+        public void OnPost(string name, int id)
+        {
+            users.Add(new WebApplication1.User(id, name));
+        }
+    }
+}
---
+++ WebApplication1/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public User(int id, string name)
+        {
+            Id= id;
+            Name= name;
+        }
+    }
+}
